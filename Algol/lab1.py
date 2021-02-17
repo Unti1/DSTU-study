7b885ee1432ec83f42de6720de77b813bd6eccb9
Unti1 <49184831+Unti1@users.noreply.github.com>
2021-02-17 03:48:17 +0300

    Добавление части задания из лабораторной

---
+++ Algol/lab1.py
@@ -0,0 +1,53 @@
+# 1.5
+#1
+A = []
+n = int(input("Сколько элементов будет в множестве ?\n"))
+
+for i in range(n):
+    A.append(input('Введите знавение\n'))
+
+for i in range(n):
+    print(f'{i}\t{A[i]}')
+
+#2
+
+word1 = 'Составить программу определения количества гласных букв'
+word2 = 'Организовать программу для задания элементов множества'
+
+array1 = word1.split(' ')
+array2 = word2.split(' ')
+
+def comparison(val1,val2):
+    array3 = []
+    if len(val1)>len(val2):
+        for i in val1:
+            if i in val2:
+                array3.append(i)
+    else:
+        for i in val2:
+            if i in val1:
+                array3.append(i)
+    return(array3)
+    
+print(comparison(array1,array2))
+
+#3
+
+S = ['a','к','ф','r',2,3,'e']
+dicRu = tuple('абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФЧЦЧШЩЪЫЬ')
+dicEn = tuple('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+A = []
+B = []
+C = []
+
+for i in S:
+    if i in dicRu:
+        A.append(i)
+    elif i in dicEn:
+        B.append(i)
+    elif type(i) == int :
+        C.append(i)
+    else:
+        print('Неопознанный словарь')
+
+print(A,B,C)
