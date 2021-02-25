8a9e88e61e3f0829387230e3cc47d431e645486d
Unti1 <49184831+Unti1@users.noreply.github.com>
2021-02-26 01:49:41 +0300

    Правка и добавление

--- Algol/lab2.4.py
+++ Algol/lab2.4.py
@@ -1,21 +1,6 @@
-import random
-
-n = 100 # число букв в строке
-N = 50 # количество строк
-i = 0
 filename = 'sym3.txt'
-dicEn = tuple(' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
-# создание текстового файла
+with open(filename,"r",encoding='utf8') as f:
+    text = f.readlines
 
-with open(filename, "w",encoding='utf8') as f:
-    for i in range(N):
-        word = ''
-        while i != n:
-            i += 1
-            word += random.choice(dicEn)
-        print(word,file=f )
-
-# функция поиска в файле введеной строке
-
-def finder(file_name,num):
+# def finder(file_name,num)
