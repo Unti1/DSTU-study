9d2f850abf6a63d44896dca6bb9b8b0e878c825b
Unti1 <49184831+Unti1@users.noreply.github.com>
2021-02-27 16:14:42 +0300

    Finaly тархун

--- Algol/lab2.4.py
+++ Algol/lab2.4.py
@@ -1,7 +1,5 @@
 import string
 
-filename = 'sym2.txt'
-
 
 def finder(filename,sym):
     with open(filename,"r",encoding='utf8') as f:
@@ -21,16 +19,28 @@ def finder(filename,sym):
         pogr = 0
     return pos
 
-# print(finder(filename,"G"))
+print(finder("sym2.txt","G"))
+
 
 def uni_word(filename):
     with open(filename,"r",encoding='utf8') as f:
         filetxs = f.readlines()
 
     u_dict = []
+    dict_of_words = []
 
     for i in range(len(filetxs)):
-        filetext = filetxs[i]
+        filetext = filetxs[i].split('\n')[0]
         filetext.translate(str.maketrans('', '', string.punctuation))
-        for j in range(len(filetext.split(' '))):
-            
+        list_of_word = filetext.split(' ')
+        for j in range(len(list_of_word)):
+            dict_of_words.append(list_of_word[j])
+
+    my_dict = {i:dict_of_words.count(i) for i in dict_of_words}
+
+    for val in my_dict.items():
+        if val[1] == 1:
+            u_dict.append(val[0])
+
+    return u_dict
+print(uni_word("sym3.txt"))
