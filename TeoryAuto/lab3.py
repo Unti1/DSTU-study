ee86126862c26c13209488b7239b28a99bc0c7e1
Unti1 <49184831+Unti1@users.noreply.github.com>
2021-03-31 23:37:16 +0300

    Финальный файл

--- TeoryAuto/lab3.py
+++ TeoryAuto/lab3.py
@@ -6,15 +6,17 @@ in_cout_list = [['ABC','AC'],
                 ['ABC','AC'],
                 ['','AC']]
 
-dot = Digraph(comment="Я ортейм кек))))))))))")
+dot = Digraph()
 for i in alflist:
     dot.node(i,i)
 
 for i in range(len(alflist)):
+    print(i)
     for val in in_cout_list[i]:
+        print(val)
         if val != '':
             v_lst = list(val)
             for sym in v_lst:
                 dot.edge(alflist[i],sym,label=countlist[0],constraint = 'false')             
 # print(dot)
-dot.render('C:/MyProjects/DPTU/TeoryAuto/testout/test_graph.gv', view=True, format="png")
+dot.render('testout/test_graph.gv', view=True, format="png")
